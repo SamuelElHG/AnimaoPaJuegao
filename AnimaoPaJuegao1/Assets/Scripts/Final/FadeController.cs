137166ca3580826638ca731a53ad893f0776f9dd
SamuelElHG <samuel.hernandezg@upb.edu.co>
2023-11-07 12:57:45 -0500

    presentable

--- AnimaoPaJuegao1/Assets/Scripts/Final/FadeController.cs
+++ AnimaoPaJuegao1/Assets/Scripts/Final/FadeController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class FadeController : MonoBehaviour
 {
     [SerializeField] public Animator FadeAnimator;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private float JumpDelay;
+    [SerializeField] private float dashForce;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +22,30 @@ public class FadeController : MonoBehaviour
         
         if (Input.anyKey)
         {
-            if (Input.GetKeyDown(KeyCode.D)) //walk
-            { FadeAnimator.SetInteger("TheInput", 1);}
+            if (Input.GetKey(KeyCode.A)) //walk foward
+            { FadeAnimator.SetInteger("TheInput", 1);
+            transform.position = transform.position+new Vector3(1*moveSpeed*Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.D)) //walk backwards
+            { FadeAnimator.SetInteger("TheInput", 1);
+                transform.position = transform.position + new Vector3(-1 * moveSpeed * Time.deltaTime, 0, 0);
+            }
 
             if (Input.GetKeyDown(KeyCode.W))//jump
-            {FadeAnimator.SetInteger("TheInput", 2);}
+            {
+                StartCoroutine(jumpCoru());
+               // FadeAnimator.SetInteger("TheInput", 2);
+                //transform.position = transform.position + new Vector3(0,1* JumpForce * Time.deltaTime, 0);
+            }
 
             if (Input.GetKeyDown(KeyCode.F)) //Punch
             { FadeAnimator.SetInteger("TheInput", 3); }
 
             if (Input.GetKeyDown(KeyCode.G))//Dash
-            { FadeAnimator.SetInteger("TheInput", 4); }
+            { FadeAnimator.SetInteger("TheInput", 4);
+                transform.position = transform.position + new Vector3(-dashForce,0, 0);
+            }
 
             if (Input.GetKeyDown(KeyCode.S)) //Crouch
             { FadeAnimator.SetInteger("TheInput", 5); }
@@ -37,5 +54,11 @@ public class FadeController : MonoBehaviour
         else FadeAnimator.SetInteger("TheInput", 0);
         
 
+    }
+    IEnumerator jumpCoru()
+    {
+        FadeAnimator.SetInteger("TheInput", 2);
+        yield return new WaitForSeconds(JumpDelay);
+        transform.position = transform.position + new Vector3(0,JumpForce, 0);
     }
 }
--- AnimaoPaJuegao1/Assets/Scripts/Final/RobotController.cs
+++ AnimaoPaJuegao1/Assets/Scripts/Final/RobotController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class RobotController : MonoBehaviour
 {
     [SerializeField] public Animator BotAnimator;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private float JumpDelay;
+    [SerializeField] private float dashForce;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +23,30 @@ public class RobotController : MonoBehaviour
 
         if (Input.anyKey)
         {
-            if (Input.GetKeyDown(KeyCode.J)) //walk
-            { BotAnimator.SetInteger("TheInput", 1); }
+            if (Input.GetKey(KeyCode.J)) //walk backwards
+            { BotAnimator.SetInteger("TheInput", 1);
+                transform.position = transform.position + new Vector3(-1 * moveSpeed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.L)) //walk foward
+            { BotAnimator.SetInteger("TheInput", 1);
+                transform.position = transform.position + new Vector3(1 * moveSpeed * Time.deltaTime, 0, 0);
+            }
 
             if (Input.GetKeyDown(KeyCode.I))//jump
-            { BotAnimator.SetInteger("TheInput", 2); }
+            {
+                StartCoroutine(jumpCoru());
+               // BotAnimator.SetInteger("TheInput", 2);
+               // transform.position = transform.position + new Vector3(0, JumpForce, 0);
+            }
 
             if (Input.GetKeyDown(KeyCode.H)) //Punch
             { BotAnimator.SetInteger("TheInput", 3); }
 
-            if (Input.GetKeyDown(KeyCode.L))//Dash
-            { BotAnimator.SetInteger("TheInput", 4); }
+            if (Input.GetKeyDown(KeyCode.M))//Dash
+            { BotAnimator.SetInteger("TheInput", 4);
+                transform.position = transform.position + new Vector3(dashForce, 0, 0);
+            }
 
             if (Input.GetKeyDown(KeyCode.K)) //Crouch
             { BotAnimator.SetInteger("TheInput", 5); }
@@ -37,5 +55,11 @@ public class RobotController : MonoBehaviour
         else BotAnimator.SetInteger("TheInput", 0);
 
 
+    }
+    IEnumerator jumpCoru()
+    {
+        BotAnimator.SetInteger("TheInput", 2);
+        yield return new WaitForSeconds(JumpDelay);
+        transform.position = transform.position + new Vector3(0, JumpForce, 0);
     }
 }
